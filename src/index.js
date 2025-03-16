868f4c49c7d1da685e15f770f66113be0d1ac49d
tjswjd0218 <tjswjd0218@gmail.com>
2025-03-16 22:37:26 +0900

    prisma_migration

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Controllers from "./controllers";
 import { swaggerDocs, options } from "./swagger";
 import swaggerUi from "swagger-ui-express";
 
+
 const app = express();
 
 // 미들웨어
@@ -16,6 +17,11 @@ app.use(express.urlencoded({ extended: true, limit: "700mb" }));
 Controllers.forEach((controller) => {
     app.use(controller.path, controller.router)
 });
+
+app.get("/swagger.json", (req, res) => {
+    res.status(200).json(swaggerDocs);
+})
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(undefined, options));
 
 app.get("/", (req, res) => {
     res.send("Nodejs 강의 재밌어요!")
